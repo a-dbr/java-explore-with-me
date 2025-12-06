f9c70a2dda378d6c7ef73ac96db42ba8d950e3d6
a-dbr <dobrolyubov92@yandex.ru>
2025-12-07 01:15:03 +0300

    Исправлен маппинг подборок

--- ewm-service/src/main/java/ru/practicum/ewm/compilation/mapper/CompilationMapper.java
+++ ewm-service/src/main/java/ru/practicum/ewm/compilation/mapper/CompilationMapper.java
@@ -1,6 +1,7 @@
 package ru.practicum.ewm.compilation.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import ru.practicum.ewm.compilation.dto.CompilationDto;
 import ru.practicum.ewm.compilation.dto.NewCompilationDto;
 import ru.practicum.ewm.compilation.model.Compilation;
@@ -9,6 +10,7 @@ import ru.practicum.ewm.event.model.Event;
 @Mapper(componentModel = "spring")
 public interface CompilationMapper {
 
+    @Mapping(target = "pinned", expression = "java(dto.getPinned() == null ? false : dto.getPinned())")
     Compilation toCompilation(NewCompilationDto dto);
 
     CompilationDto toCompilationDto(Compilation compilation);
