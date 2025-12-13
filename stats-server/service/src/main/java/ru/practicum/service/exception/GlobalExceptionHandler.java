46397328bb796776314f25879f6f1380ecbaa411
a-dbr <dobrolyubov92@yandex.ru>
2025-12-14 02:36:24 +0300

    Исправлена обработка ошибок

--- stats-server/service/src/main/java/ru/practicum/service/exception/GlobalExceptionHandler.java
+++ stats-server/service/src/main/java/ru/practicum/service/exception/GlobalExceptionHandler.java
@@ -2,12 +2,14 @@ package ru.practicum.service.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.practicum.dto.ApiErrorDto;
 
 import java.time.LocalDateTime;
@@ -15,7 +17,8 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.List;
 
-@ControllerAdvice
+@RestControllerAdvice
+@Order(Ordered.LOWEST_PRECEDENCE)
 public class GlobalExceptionHandler {
 
     private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
